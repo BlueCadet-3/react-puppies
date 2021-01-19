28525615f3c625c0d14d8210e9112abb7e983314
BlueCadet-3 <martysilha@gmail.com>
2021-01-19 16:57:53 -0600

    part 2

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -9,24 +9,24 @@ import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage";
 export default function App() {
   const [user, setUser] = useState({});
 
-return (
-  <main className="App">
-    {user ? (
-      <>
-        <NavBar />
-        <Switch>
-          <Route path="/orders/new">
-            <NewOrderPage />
-          </Route>
-          <Route path="/orders">
-            <OrderHistoryPage />
-          </Route>
-          <Redirect to="/orders" />
-        </Switch>
-      </>
-    ) : (
-      <AuthPage />
-    )}
-  </main>
-);
+  return (
+    <main className="App">
+      {user ? (
+        <>
+          <NavBar />
+          <Switch>
+            <Route path="/orders/new">
+              <NewOrderPage />
+            </Route>
+            <Route path="/orders">
+              <OrderHistoryPage />
+            </Route>
+            <Redirect to="/orders" />
+          </Switch>
+        </>
+      ) : (
+        <AuthPage />
+      )}
+    </main>
+  );
 }
